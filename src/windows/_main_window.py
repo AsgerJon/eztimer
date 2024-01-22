617d33688caa5bd2b5a2b1c52cd429be3aaae2cd
Asger Jon Vistisen <asgerjon2@gmail.com>
2024-01-22 09:37:48 +0100

    Media player source

--- src/windows/_main_window.py
+++ src/windows/_main_window.py
@@ -3,10 +3,12 @@
 #  Copyright (c) 2024 Asger Jon Vistisen
 from __future__ import annotations
 
-from PySide6.QtCore import QTimer
+import os
+
+from PySide6.QtCore import QTimer, QUrl
 from PySide6.QtMultimedia import QMediaPlayer, QMediaDevices, QAudioOutput
 
-from windows import LayoutWindow
+from windows import __project_root__, LayoutWindow
 
 
 class MainWindow(LayoutWindow):
@@ -14,12 +16,14 @@ class MainWindow(LayoutWindow):
 
   def __init__(self, *args, **kwargs) -> None:
     LayoutWindow.__init__(self, *args, **kwargs)
+    #  Timers
     self._fullTimer = QTimer()
     self._fullTimer.setSingleShot(True)
-    self._fullTimer.timeout.connect(self._timeOutFunc)
     self._incrementTimer = QTimer()
     self._incrementTimer.setInterval(100)
     self._incrementTimer.setSingleShot(False)
+    #  Actions and signals
+    self._fullTimer.timeout.connect(self._timeOutFunc)
     self._incrementTimer.timeout.connect(self._timerUpdateFunc)
     self._startButton.clicked.connect(self._startFunc)
     self._cancelButton.clicked.connect(self._cancelFunc)
@@ -30,12 +34,18 @@ class MainWindow(LayoutWindow):
     self._audioDevice = QMediaDevices.defaultAudioOutput()
     self._audioOutput = QAudioOutput(self._audioDevice)
     self._mediaPlayer.setAudioOutput(self._audioOutput)
-    here = __project_root__
-    self._audioFile = 'rickroll.mp3ttttttttttttttt'
+
+  def _setMediaSource(self) -> None:
+    """Sets up the source for the media player"""
+    filePath = None
+    for item in os.listdir(__project_root__):
+      if os.path.splitext(item)[-1] in ['mp3'] and filePath is None:
+        filePath = os.path.join(__project_root__, item)
+        self._mediaPlayer.setSource((QUrl.fromLocalFile(filePath)))
 
   def _timerUpdateFunc(self) -> None:
     """Handles notification of timer update"""
-
+    
   def _startFunc(self, ) -> None:
     """Starts the timer"""
 
